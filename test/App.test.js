093ae7e05b54b4f31c9c7ef1c0cdee2462def909
James Cockbain <james.cockbain@ibm.com>
2019-10-26 13:01:07 +0100

    setup babel and jest

---
+++ test/App.test.js
@@ -0,0 +1,10 @@
+import App from "../src/components/App";
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("<App />", () => {
+  test("should have correct header", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toBe("App");
+  });
+});
