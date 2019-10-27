4b4a1e7d27ddbcd2c9086da04f0b027d70e753fb
James Cockbain <james.cockbain@ibm.com>
2019-10-27 19:22:44 +0000

    fix linter warnings

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,33 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./src/index.js",
+    main: './src/index.js',
   },
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve("./dist"),
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: ["/node_modules"],
-        use: [{ loader: "babel-loader" }],
+        exclude: ['/node_modules'],
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.s(a|c)ss/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
@@ -35,7 +35,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
     new CleanWebpackPlugin(),
   ],
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.commmon.js");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
   devServer: {
-    host: "localhost",
-    port: "3000",
+    host: 'localhost',
+    port: '3000',
     open: true,
   },
 });
